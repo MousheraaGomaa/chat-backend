1acc76fcab5501fbabe4afeede97604ddf024e5c
MousheraaGomaa <mousheraa.gomaa@gmail.com>
2025-04-07 23:54:20 +0200

    Update swagger.js

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -2,6 +2,7 @@ import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import { fileURLToPath } from "url";
 import path from "path";
+import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 
 // import swaggerDocument from './swagger.json';
 const __filename = fileURLToPath(import.meta.url);
@@ -9,14 +10,19 @@ const __dirname = path.dirname(__filename);
 
 const swaggerUICss =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css";
+const options = {
+customCssUrl: swaggerUICss,
+};
 
 const swaggerDocument = YAML.load(`${__dirname}/swaggerDoc.yml`);
 const generateSwaggerApiDoc = (app) => {
-  app.use(
-    "/api/api-docs",
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
-  );
+  // app.use(
+  //   "/api/api-docs",
+  //   swaggerUi.serve,
+  //   swaggerUi.setup(swaggerDocument)
+  // );
+  app.use('/api/api-docs', swaggerUi.serveFiles(swaggerDocument, options), swaggerUi.setup(swaggerDocument, options));
+
 };
 
 export default generateSwaggerApiDoc;
